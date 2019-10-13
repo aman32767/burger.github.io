09f062cef3925e0ba7e390b3e2af6b84cf7afb2b
aman <writetoaman99@gmail.com>
2019-10-13 05:46:39 +0530

    .

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,7 +4,7 @@ import NavigationItem from '../NavigationItem/NavigationItem'
 
 const navigationItems = ()=> (
     <ul className={classes.NavigationItems}>
-        <NavigationItem link="/burger" exact>NEW BURGER</NavigationItem>
+        <NavigationItem link="/burger" >NEW BURGER</NavigationItem>
         <NavigationItem link="/orders">Orders</NavigationItem>
     </ul>
 )
